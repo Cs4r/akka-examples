f356c59725aaaadccddea3ef2cfcdc50923bd560
Cesar Aguilera <cesar.aguilera.p@gmail.com>
2016-04-04 17:27:44 +0200

    sending ints

---
+++ src/main/java/cs4r/labs/akka/examples/FrameParser.java
@@ -0,0 +1,94 @@
+package cs4r.labs.akka.examples;
+
+import java.nio.ByteOrder;
+
+import akka.stream.Attributes;
+import akka.stream.FlowShape;
+import akka.stream.Inlet;
+import akka.stream.Outlet;
+import akka.stream.stage.AbstractInHandler;
+import akka.stream.stage.AbstractOutHandler;
+import akka.stream.stage.GraphStage;
+import akka.stream.stage.GraphStageLogic;
+import akka.util.ByteString;
+
+public class FrameParser extends GraphStage<FlowShape<ByteString, ByteString>> {
+	public Inlet<ByteString> in = Inlet.create("FrameParser.in");
+	public Outlet<ByteString> out = Outlet.create("FrameParser.out");
+	private FlowShape<ByteString, ByteString> shape = FlowShape.of(in, out);
+
+	@Override
+	public FlowShape<ByteString, ByteString> shape() {
+		return shape;
+	}
+
+	@Override
+	public GraphStageLogic createLogic(Attributes inheritedAttributes) {
+		return new GraphStageLogic(shape) {
+
+			// this holds the received but not yet parsed bytes
+			private ByteString stash = ByteString.empty();
+			// this holds the current message length or -1 if at a boundary
+			private int needed = -1;
+
+			{
+				setHandler(in, new AbstractInHandler() {
+					@Override
+					public void onPush() throws Exception {
+						ByteString bytes = grab(in);
+						stash = stash.concat(bytes);
+						run();
+					}
+
+					@Override
+					public void onUpstreamFinish() throws Exception {
+						if (stash.isEmpty())
+							completeStage();
+						// wait with completion and let run() complete when the
+						// rest of the stash has been sent downstream
+					}
+				});
+
+				setHandler(out, new AbstractOutHandler() {
+					@Override
+					public void onPull() throws Exception {
+						if (isClosed(in))
+							run();
+						else
+							pull(in);
+					}
+				});
+			}
+
+			private void run() {
+				if (needed == -1) {
+					// are we at a boundary? then figure out next length
+					if (stash.size() < 4) {
+						if (isClosed(in))
+							completeStage();
+						else
+							pull(in);
+					} else {
+						needed = stash.iterator().getInt(ByteOrder.LITTLE_ENDIAN);
+						stash = stash.drop(4);
+						run(); // cycle back to possibly already emit the next
+								// chunk
+					}
+				} else if (stash.size() < needed) {
+					// we are in the middle of a message, need more bytes
+					// or in is already closed and we cannot pull any more
+					if (isClosed(in))
+						completeStage();
+					else
+						pull(in);
+				} else {
+					// we have enough to emit at least one message, so do it
+					final ByteString emit = stash.take(needed);
+					stash = stash.drop(needed);
+					needed = -1;
+					push(out, emit);
+				}
+			}
+		};
+	}
+}
--- src/main/java/cs4r/labs/akka/examples/http/TcpEcho.java
+++ src/main/java/cs4r/labs/akka/examples/http/TcpEcho.java
@@ -2,23 +2,50 @@ package cs4r.labs.akka.examples.http;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.dispatch.OnFailure;
+import akka.dispatch.OnSuccess;
 import akka.stream.ActorMaterializer;
+import akka.stream.Attributes;
+import akka.stream.FlowShape;
+import akka.stream.Inlet;
+import akka.stream.Outlet;
+import akka.stream.javadsl.BidiFlow;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import akka.stream.javadsl.Tcp;
 import akka.stream.javadsl.Tcp.IncomingConnection;
 import akka.stream.javadsl.Tcp.ServerBinding;
+import akka.stream.stage.AbstractInHandler;
+import akka.stream.stage.AbstractOutHandler;
+import akka.stream.stage.GraphStage;
+import akka.stream.stage.GraphStageLogic;
+import akka.util.ByteIterator;
 import akka.util.ByteString;
+import akka.util.ByteStringBuilder;
+import cs4r.labs.akka.examples.BidiFlows;
+import cs4r.labs.akka.examples.BidiFlows.Message;
+import scala.concurrent.Future;
+import scala.runtime.BoxedUnit;
+import cs4r.labs.akka.examples.FrameParser;
 
 public class TcpEcho {
+
+	private static Flow<Message, ByteString, NotUsed> codec = Flow.of(Message.class).map(TcpEcho::toBytes)
+			.map(TcpEcho::addLengthHeader);
+
+	private static Flow<ByteString, Message, NotUsed> decodec = Flow.of(ByteString.class).map(TcpEcho::fromBytes);
+
+	private static Flow<ByteString, ByteString, NotUsed> framing = Flow.of(ByteString.class).via(new FrameParser());
 
 	/**
 	 * Use without parameters to start both client and server.
@@ -31,6 +58,7 @@ public class TcpEcho {
 	 *
 	 */
 	public static void main(String[] args) throws IOException {
+
 		if (args.length == 0) {
 			ActorSystem system = ActorSystem.create("ClientAndServer");
 			InetSocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 6000);
@@ -58,8 +86,9 @@ public class TcpEcho {
 
 		final Sink<IncomingConnection, CompletionStage<Done>> handler = Sink.foreach(conn -> {
 			System.out.println("Client connected from: " + conn.remoteAddress());
-
-			Flow<ByteString, ByteString, NotUsed> create = Flow.<ByteString> create();
+			
+			Flow<ByteString, ByteString, NotUsed> create = Flow.of(ByteString.class).via(decodec)
+					.<Message> map(m -> new Pong(((Ping) m).id)).via(codec).via(framing);
 
 			conn.handleWith(create, materializer);
 		});
@@ -82,28 +111,98 @@ public class TcpEcho {
 	public static void client(ActorSystem system, InetSocketAddress serverAddress) {
 		final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-		final List<ByteString> testInput = new ArrayList<>();
-		for (char c = 'a'; c <= 'z'; c++) {
-			testInput.add(ByteString.fromString(String.valueOf(c)));
-		}
+		Source<Message, NotUsed> source = Source.from(Arrays.asList(0, 1, 2)).<Message> map(id -> new Ping(id));
 
-		Source<ByteString, NotUsed> responseStream = Source.from(testInput)
+		Source<ByteString, NotUsed> encodedAndFramed = source.via(codec).via(framing);
+
+		Source<ByteString, NotUsed> responseStream = encodedAndFramed
 				.via(Tcp.get(system).outgoingConnection(serverAddress.getHostString(), serverAddress.getPort()));
 
-		CompletionStage<ByteString> result = responseStream.runFold(ByteString.empty(), (acc, in) -> acc.concat(in),
-				materializer);
+		Source<Message, NotUsed> integers = responseStream.via(decodec);
 
-		result.whenComplete((success, failure) -> {
+		integers.to(Sink.foreach(e -> System.out.println(e))).run(materializer);
+	}
 
-			if (failure != null) {
-				System.err.println("Failure: " + failure.getMessage());
-			} else {
-				System.out.println("Result: " + success.utf8String());
-			}
-			System.out.println("Shutting down client");
-			system.terminate();
+	public static ByteString addLengthHeader(ByteString bytes) {
+		final int len = bytes.size();
+		return new ByteStringBuilder().putInt(len, ByteOrder.LITTLE_ENDIAN).append(bytes).result();
+	}
 
-		});
+	public static ByteString toBytes(Message msg) {
+		if (msg instanceof Ping) {
+			final int id = ((Ping) msg).id;
+			return new ByteStringBuilder().putByte((byte) 1).putInt(id, ByteOrder.LITTLE_ENDIAN).result();
+		} else {
+			final int id = ((Pong) msg).id;
+			return new ByteStringBuilder().putByte((byte) 2).putInt(id, ByteOrder.LITTLE_ENDIAN).result();
+		}
+	}
+
+	public static Message fromBytes(ByteString bytes) {
+		final ByteIterator it = bytes.iterator();
+		switch (it.getByte()) {
+		case 1:
+			return new Ping(it.getInt(ByteOrder.LITTLE_ENDIAN));
+		case 2:
+			return new Pong(it.getInt(ByteOrder.LITTLE_ENDIAN));
+		default:
+			throw new RuntimeException("message format error");
+		}
+	}
+
+	static interface Message {
+	}
+
+	static class Ping implements Message {
+		final int id;
+
+		public Ping(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof Ping) {
+				return ((Ping) o).id == id;
+			} else
+				return false;
+		}
+
+		@Override
+		public int hashCode() {
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return "Ping " + String.valueOf(id);
+		}
+	}
+
+	static class Pong implements Message {
+		final int id;
+
+		public Pong(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof Pong) {
+				return ((Pong) o).id == id;
+			} else
+				return false;
+		}
+
+		@Override
+		public int hashCode() {
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return "Pong " + String.valueOf(id);
+		}
 	}
 
 }
